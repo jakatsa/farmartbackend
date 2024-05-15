d6526a1fb534c57d2a85c9b251d16d07b5796f0f
jakatsa <118554412+jakatsa@users.noreply.github.com>
2024-05-15 09:54:03 +0300

    add views/joe (#9)

--- api/views.py
+++ api/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from api.models import User, Customer, Farmer, Animal,Orders
+from api.serializers import (
+    UserSerializer,
+    FarmerSerializer,
+    CustomerSerializer,
+    AnimalSerializer,
+    OrderSerializer,
+)
+from django.contrib.auth import authenticate, login
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 
-# Create your views here.
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AnimalViewing(generics.ListCreateAPIView):
+    queryset = Animal.objects.all()
+    serializer_class = AnimalSerializer
+
+
+class CustomerRegistrationView(APIView):
+    def post(self, request):
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FarmerRegistrationView(APIView):
+    def post(self, request):
+        serializer = FarmerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class StudentRegistrationView(APIView):
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
