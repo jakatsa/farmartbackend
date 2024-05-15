ff17e71131688f35a5f81a62efd381f19136670d
terence <terenceshawn18@outlook.com>
2024-05-15 08:54:30 +0300

    added imports

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,13 @@
 from rest_framework import serializers
 from .models import User, Customer, Farmer, Animal, Orders
 from django.utils import timezone
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "email", "role", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
