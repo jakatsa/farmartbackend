93bb214f47295d7501a57e90f80e0105333e0993
terence <terenceshawn18@outlook.com>
2024-05-15 08:56:26 +0300

    added customer serializer

--- api/serializers.py
+++ api/serializers.py
@@ -11,3 +11,22 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+class CustomerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()  
+
+    class Meta:
+        model = Customer
+        fields = "__all__"
+
+    def create(self, validated_data):
+        user_data = validated_data.pop("user")  
+        user_serializer = UserSerializer(data=user_data)
+        if user_serializer.is_valid():
+            user = user_serializer.save()  
+            customer = Customer.objects.create(
+                user=user, **validated_data
+            ) 
+            return customer
+        else:
+            raise serializers.ValidationError(user_serializer.errors)
