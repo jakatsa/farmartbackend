c216bb31b68aa4f847f18939c8140a9b3a76d0c4
terence <terenceshawn18@outlook.com>
2024-05-15 08:57:57 +0300

    added farmer and animal serializer

--- api/serializers.py
+++ api/serializers.py
@@ -30,3 +30,47 @@ class CustomerSerializer(serializers.ModelSerializer):
             return customer
         else:
             raise serializers.ValidationError(user_serializer.errors)
+
+
+class FarmerSerializer(serializers.ModelSerializer):
+    user = UserSerializer() 
+
+    class Meta:
+        model = Farmer
+        fields = "__all__"
+
+    def create(self, validated_data):
+        user_data = validated_data.pop("user") 
+        user_serializer = UserSerializer(data=user_data)
+        if user_serializer.is_valid():
+            user = user_serializer.save() 
+            farmer = Farmer.objects.create(
+                user=user, **validated_data
+            )  
+            return farmer
+        else:
+            raise serializers.ValidationError(user_serializer.errors)
+
+
+class AnimalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Animal
+        fields = "__all__"
+
+    def create(self, validated_data):
+        
+        user = self.context["request"].user
+
+        
+        if user.role == "farmer":
+            
+            if "farmer" not in validated_data:
+                raise serializers.ValidationError("A farmer must be specified.")
+
+            
+            farmer = validated_data.pop("farmer")
+            animal = Animal.objects.create(farmer=farmer, **validated_data)
+            return animal
+        else:
+            
+            raise serializers.ValidationError("Only farmers can create animals.")
