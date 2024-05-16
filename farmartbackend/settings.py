06301e2340247c256d4c0e98a0ebd0134d6aa37d
Joe <jakatsa5@gmail.com>
2024-05-16 09:20:06 +0300

    cors added/joe

--- farmartbackend/settings.py
+++ farmartbackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8=*3u5+vebaka^-xw9il6d=$!_p73s%1xk4n+gyb*b$)w#qv*+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'api',
+      "corsheaders",
 ]
 AUTH_USER_MODEL = "api.User"
 REST_FRAMEWORK = {
@@ -49,6 +50,8 @@ REST_FRAMEWORK = {
     ],
 }
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",#cors
+    "django.middleware.common.CommonMiddleware",#cors
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,3 +136,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS=True #cors
