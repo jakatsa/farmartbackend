513bafd05bb55abd6f73bd32df3d7a1007ef6e69
terence <terenceshawn18@outlook.com>
2024-05-20 10:52:07 +0300

    updated orderserializer

--- farmartbackend/urls.py
+++ farmartbackend/urls.py
@@ -10,18 +10,25 @@ from api.views import (
     AnimalViewing,
     CreateOrderView,
     OrderListView,
-    OrderAcceptView
+    OrderAcceptView,
+    UserProfileView,
+    AnimalViewingFarmer
 )
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/user/profile",UserProfileView.as_view(),name="profile"),
     path("api/animals/", AnimalViewing.as_view(), name="animals"),
     path("api/orders/create/", CreateOrderView.as_view(), name="order-create"),
     path("api/orders/", OrderListView.as_view(), name="order-list"),
     path("api/orders/accept/<int:pk>/", OrderAcceptView.as_view(), name="order-accept"),
-   
+    path("api/animals/farmer/", AnimalViewingFarmer.as_view(), name="animals-farmer"),
+
+    #path(
+       # "api/auth/register/", UserRegistrationView.as_view(), name="user-registration"
+   # ),
     path("api/animals/add/", AnimalCreationView.as_view(), name="animal-creation"),
     path("api/auth/login/", UserLoginView.as_view(), name="user-login"),
     path("api/auth/logout/", UserLogoutView.as_view(), name="user-logout"),
@@ -35,7 +42,7 @@ urlpatterns = [
         CustomerRegistrationView.as_view(),
         name="customer-registration",
     ),
-  
+    # Add other URLs here
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
