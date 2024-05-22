bb9c94d8e0bc2c0d561a72e4e150d15388f4b651
shawn-terence <145427330+shawn-terence@users.noreply.github.com>
2024-05-22 17:23:58 +0300

    Terence (#23)
    * added requirements file
    
    * added podtgre
    
    * updating admin side

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Farmer, Customer, User, Animal,Orders
 
 # Register your models here.
+admin.site.register(Farmer)
+admin.site.register(Customer)
+admin.site.register(User)
+admin.site.register(Animal)
