d333872a1d6a67f06b2a2be0835cac1e1b5f12b1
waltersimekha <126678859+waltersimekha20@users.noreply.github.com>
2024-05-26 16:05:52 +0000

    Add get route

--- api/views.py
+++ api/views.py
@@ -257,6 +257,11 @@ class CartDetails(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        products = Cart.objects.all()
+        serializer = CartSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 
